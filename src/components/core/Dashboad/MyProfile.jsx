972b1a2923c8a4bc9ed8f4167f06a836566304dd
Sultan00786 <treeloversultan0987@gmail.com>
2024-04-12 14:17:48 +0530

    I done to create OpenRoute and PrivateRout which used for user specific routing

--- src/components/core/Dashboad/MyProfile.jsx
+++ src/components/core/Dashboad/MyProfile.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Iconbtn from "../../common/Iconbtn";
+import { VscEdit } from "react-icons/vsc";
 
 function MyProfile() {
+  const [user] = useSelector((state) => state.profile);
+  const navigate = useNavigate();
+
   return (
-    <div>MyProfile</div>
-  )
+    <div>
+      <h1>My Profile</h1>
+      <div>
+        <div>
+          <imp
+            src={user?.image}
+            alt={`profile-${user?.fristName}`}
+            className=" aspect-square w-[78px] rounded-full object-cover  "
+          />
+          <div>
+            <p> {user?.fristName + " " + user?.lastName} </p>
+            <p> {user?.email} </p>
+          </div>
+        </div>
+
+        <Iconbtn
+          text="Edit"
+          onClick={() => {
+            navigate("/dashboard/settings");
+          }}
+          children={<VscEdit />}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default MyProfile
+export default MyProfile;
