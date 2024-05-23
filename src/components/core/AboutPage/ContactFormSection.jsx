53a12b099e89c53ef99c6387f5cee60e9ee66042
Sultan00786 <treeloversultan0987@gmail.com>
2024-05-23 11:46:01 +0530

    I have done to create /dashboard/my-profile by  using outlate  nested routing. And I also solve restore  bug of user info which present in local storage

--- src/components/core/AboutPage/ContactFormSection.jsx
+++ src/components/core/AboutPage/ContactFormSection.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ContactUsForm from '../../contactPage/ContactUsForm'
+import React from "react";
+import ContactUsForm from "../../contactPage/ContactUsForm";
 
 function ContactFormSection() {
   return (
-    <div className=' w-fit mx-auto' >
-        <h1>Get in Touch</h1>
-        <p>
-            We'd love to here for you, Please fill out this form.
-        </p>
-        <div>
-            <ContactUsForm/>
-        </div>
+    <div className=" w-fit mx-auto">
+      <h1 className=" text-4xl text-richblack-25 font-bold text-center mt-40 mb-3 ">
+        Get in Touch
+      </h1>
+      <p className=" text-[17px] text-richblack-200 text-center mb-20 ">
+        We'd love to here for you, Please fill out this form.
+      </p>
+      <div>
+        <ContactUsForm />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactFormSection
+export default ContactFormSection;
--- src/components/core/Dashboad/MyProfile.jsx
+++ src/components/core/Dashboad/MyProfile.jsx
@@ -2,36 +2,132 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Iconbtn from "../../common/Iconbtn";
-import { VscEdit } from "react-icons/vsc";
+import { VscAdd, VscEdit, VscPulse, Vscplus } from "react-icons/vsc";
+import Botton from "../Homepage/Botton";
 
 function MyProfile() {
-  const [user] = useSelector((state) => state.profile);
+  const { user } = useSelector((state) => state.profile);
+  console.log("user1: ", user?.additionalDetails?.about);
+
+  const { image } = user;
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>My Profile</h1>
-      <div>
+    <div className=" text-white w-full flex flex-col gap-9 ">
+      {/* Section 1 */}
+      <section>
+        <h1>My Profile</h1>
         <div>
-          <imp
-            src={user?.image}
-            alt={`profile-${user?.fristName}`}
-            className=" aspect-square w-[78px] rounded-full object-cover  "
-          />
           <div>
-            <p> {user?.fristName + " " + user?.lastName} </p>
-            <p> {user?.email} </p>
+            <img
+              src={image}
+              className=" rounded-full w-[78px] aspect-square object-cover "
+            />
+            <div>
+              <p> {user?.firstName + " " + user?.lastName} </p>
+              <p> {user?.email} </p>
+            </div>
+          </div> 
+
+          <div className=" -mt-8 w-fit  ">
+            <Botton active={true} linkto="/dashboard/settings">
+              <div className="flex flex-row items-center text-[16px] gap-x-2">
+                <p>Edit</p>
+                <VscEdit />
+              </div>
+            </Botton>
+          </div>
+        </div>
+      </section>
+
+      {/* Section 2 */}
+      {/* api call nahi ho rahi --> additionalDetails undifined b  aa raha hie */}
+      <section>
+        <div>
+          <p>About</p>
+          <div className=" -mt-8 w-fit  ">
+            <Botton active={true} linkto="/dashboard/settings">
+              <div className="flex flex-row items-center text-[16px] gap-x-2">
+                <p>Edit</p>
+                <VscEdit />
+              </div>
+            </Botton>
+          </div>
+        </div>
+        <p>
+          {user?.additionalDetails?.about
+            ? user?.additionalDetails?.about
+            : "Write something about Yourself"}
+        </p>
+      </section>
+
+      {/* Section 3 */}
+      <section>
+        <div className=" flex flex-row items-center justify-between ">
+          <p>Personal Details</p>
+          <div className=" -mt-8 w-fit ">
+            <Botton active={true} linkto="/dashboard/settings">
+              <div className="flex flex-row items-center text-[16px] gap-x-2">
+                <p>Edit</p>
+                <VscEdit />
+              </div>
+            </Botton>
           </div>
         </div>
 
-        <Iconbtn
-          text="Edit"
-          onClick={() => {
-            navigate("/dashboard/settings");
-          }}
-          children={<VscEdit />}
-        />
-      </div>
+        <div className=" grid grid-cols-2 grid-rows-3 gap-6">
+          <div>
+            <p>First Name</p>
+            <p> {user?.firstName} </p>
+          </div>
+
+          <div>
+            <p>Last Name</p>
+            <p> {user?.lastName} </p>
+          </div>
+
+          <div>
+            <p>Email</p>
+            <p> {user?.email} </p>
+          </div>
+
+          <div>
+            <p>Phone Number</p>
+            <p>
+              {" "}
+              {user?.additionalDetails?.contactNamber ?? (
+                <span className="flex flex-row items-center gap-3">
+                  <VscAdd /> Add Phone Number
+                </span>
+              )}{" "}
+            </p>
+          </div>
+
+          <div>
+            <p>Gender</p>
+            <p>
+              {" "}
+              {user?.additionalDetails?.gender ?? (
+                <span className="flex flex-row items-center gap-3">
+                  <VscAdd /> Add Gender
+                </span>
+              )}{" "}
+            </p>
+          </div>
+
+          <div>
+            <p>Date of Birth</p>
+            <p>
+              {" "}
+              {user?.additionalDetails?.dateOfBirth ?? (
+                <span className="flex flex-row items-center gap-3">
+                  <VscAdd /> Add DOB
+                </span>
+              )}{" "}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
