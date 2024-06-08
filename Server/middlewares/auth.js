96e15a4bb7eab89622c7452bd988dede7bd0cfeb
Sultan00786 <treeloversultan0987@gmail.com>
2024-06-08 20:12:07 +0530

    conflict resolve in \Server\middlewares

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -16,12 +16,6 @@ exports.auth = async (req, res, next) => {
     const token = await (req.cookies.token ||
       req.body.token ||
       req.header("Authorisation").replace("Bearer ", ""));
-
-<<<<<<< HEAD
-=======
-    console.log("token is: ", token);
-
->>>>>>> 7953e65eac7bf48d4a32f70a1e4bdc97f2183dc7
     // check if token missing
     if (!token) {
       return res.status(401).json({
@@ -33,11 +27,6 @@ exports.auth = async (req, res, next) => {
     // verify the token
     try {
       const decode = jwt.verify(token, process.env.JWT_SECRET);
-<<<<<<< HEAD
-=======
-      console.log("Before decode");
-      console.log(decode);
->>>>>>> 7953e65eac7bf48d4a32f70a1e4bdc97f2183dc7
       req.user = decode;
     } catch (error) {
       console.log(error);
