dc1885d92490242ba4a8bbe57e102e968373cf69
Sultan00786 <treeloversultan0987@gmail.com>
2024-06-08 22:12:18 +0530

    conflict resolve in \src\reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
 import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice";
-<<<<<<< HEAD
 import courseReducer from "../slices/courseSlice";
 import viewCourseReducer from "../slices/viewCourseSlice";
 
@@ -15,14 +14,3 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
-=======
-
-const rootReducer = combineReducers ({
-    auth: authReducer,
-    profile: profileReducer,
-    cart: cartReducer,
-});
-
-export default rootReducer;
-
->>>>>>> 7953e65eac7bf48d4a32f70a1e4bdc97f2183dc7
