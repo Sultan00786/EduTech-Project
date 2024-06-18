d3a5be7b1f8859d7959a0d56ed41e5627115abb3
Sultan00786 <treeloversultan0987@gmail.com>
2024-06-18 20:29:31 +0530

    fixes:issue of totalStudent and totalIncome rendering values:

--- src/components/core/Dashboad/InstructorDashboard/Instructor.jsx
+++ src/components/core/Dashboad/InstructorDashboard/Instructor.jsx
@@ -4,12 +4,15 @@ import { fetchInstructorCourses } from "../../../../services/operations/courseDe
 import { getInstructorData } from "../../../../services/operations/profileAPI";
 import Hi_logo from "../../../../assets/Logo/hi_emoji_png.webp";
 import { Link, useNavigate } from "react-router-dom";
+import InstructorPieChart from "./InstructorPieChart";
 
 function Instructor() {
   const { token } = useSelector((state) => state.auth);
   const { user } = useSelector((state) => state.profile);
   const [loading, setLoading] = useState(false);
   const [instructorData, setInstructorData] = useState(null);
+  const [totalStudent, setTotalStudent] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
   const [courses, setCourses] = useState([]);
   const navigate = useNavigate();
 
@@ -18,15 +21,35 @@ function Instructor() {
       setLoading(true);
       const instructorApiData = await getInstructorData(token);
       const result = await fetchInstructorCourses(token);
-      console.log("instructorApiData >>", instructorApiData);
       // console.log("result >>", result);
 
-      if (instructorApiData.length) setInstructorData(instructorApiData);
-      if (result) setCourses(result?.slice(0, 3));
+      if (result) setCourses(result.slice(0, 3));
+      if (instructorApiData?.length) {
+        setInstructorData(instructorApiData);
+
+        let allTotalStudent = 0;
+        let allTotalIncome = 0;
+
+        for (let i = 0; i < instructorApiData.length; i++) {
+          allTotalStudent += instructorApiData[i]?.totalStudentsEnrolled;
+          allTotalIncome += instructorApiData[i]?.totalAmountGenerated;
+        }
+
+        setTotalIncome(allTotalIncome);
+        setTotalStudent(allTotalStudent);
+      }
       setLoading(false);
     };
     dataStateFunction();
   }, []);
+
+  if (loading) {
+    return (
+      <div className=" w-full h-full flex items-center justify-center">
+        <div className="loader"></div>
+      </div>
+    );
+  }
 
   return (
     <div className=" flex flex-col gap-2 text-white">
@@ -40,7 +63,40 @@ function Instructor() {
         <p className=" text-richblack-300 text-lg">Let's Start something new</p>
       </div>
 
-      <div></div>
+      <div className=" flex gap-2">
+        <div className="bg-richblack-800 w-full rounded-lg p-5">
+          <InstructorPieChart instructorData={instructorData} />
+        </div>
+        <div className=" bg-richblack-800 rounded-lg p-5 ">
+          <div className=" w-[170px] flex flex-col justify-center gap-2">
+            <p className=" text-richblack-5 text-xl font-bold">Statistic</p>
+            <div className="flex flex-col ">
+              <p className=" text-richblack-400 font-semibold -mb-1">
+                Total Course
+              </p>
+              <p className=" text-richblack-200 text-3xl font-bold">
+                {instructorData?.length}
+              </p>
+            </div>
+            <div>
+              <p className=" text-richblack-400 font-semibold -mb-1">
+                Total Student
+              </p>
+              <p className=" text-richblack-200 text-3xl font-bold">
+                {totalStudent}
+              </p>
+            </div>
+            <div>
+              <p className=" text-richblack-400 font-semibold -mb-1">
+                Total Income
+              </p>
+              <p className=" text-richblack-200 text-3xl font-bold">
+                Rs.{totalIncome}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div className=" bg-richblack-800 rounded-lg p-5">
         <div className=" flex items-center justify-between mb-3">
