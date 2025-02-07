f713c70d3de374058a75a010ba2f1f301fa3c0b3
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-02-07 15:30:21 +0530

    fixs: ui of VerifyEmail.jsx

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -9,96 +9,121 @@ import { Link } from "react-router-dom";
 import { FaArrowLeftLong } from "react-icons/fa6";
 
 const VerifyEmail = () => {
-  const { loading, signupData } = useSelector((state) => state.auth); // using redux
-  const [otp, setOtp] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+   const { loading, signupData } = useSelector((state) => state.auth); // using redux
+   const [otp, setOtp] = useState("");
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
 
-  // checking for sign up data is present or not ??
-  useEffect(() => {
-    if (!signupData) navigate("/signup");
-  }, []);
+   // checking for sign up data is present or not ??
+   useEffect(() => {
+      if (!signupData) navigate("/signup");
+   }, []);
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    const {
-      accountType,
-      firstName,
-      lastName,
-      email,
-      password,
-      confirmPassword,
-    } = signupData;
+   const submitHandler = (event) => {
+      event.preventDefault();
+      const {
+         accountType,
+         firstName,
+         lastName,
+         email,
+         password,
+         confirmPassword,
+      } = signupData;
 
-    // console.log("signupData --> ", signupData, "\n", otp);
+      // console.log("signupData --> ", signupData, "\n", otp);
 
-    dispatch(
-      signUp(
-        accountType,
-        firstName,
-        lastName,
-        email,
-        password,
-        confirmPassword,
-        otp,
-        navigate
-      )
-    );
-  };
+      dispatch(
+         signUp(
+            accountType,
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            otp,
+            navigate
+         )
+      );
+   };
 
-  return (
-    <div className=" w-full h-[85vh] flex justify-center items-center mt-7 text-richblack-50 text-[16px] select-none ">
-      {loading ? (
-        <div className=" w-[100vw] h-[90vh] flex flex-row mx-auto justify-center items-center">
-          <span className="loader"></span>
-        </div>
-      ) : (
-        <div className=" w-[508px] flex flex-col py-2 px-9  ">
-          <h1 className=" text-3xl text-richblack-5 font-semibold mb-3 ">
-            Verify Email
-          </h1>
-          <p className=" text-[17px] text-richblack-200 mb-3">
-            A verification code has been sent to you. Enter the code below
-          </p>
-
-          <form onSubmit={submitHandler}>
-            <div>
-              <OtpInput
-                value={otp}
-                onChange={setOtp}
-                numInputs={6}
-                renderSeparator={<span className=" p-2 "></span>}
-                renderInput={(props) => (
-                  <input {...props} className=" w-[40px] bg-richblack-800 " />
-                )}
-              />
+   return (
+      <div className=" w-full h-[85vh] flex justify-center items-center mt-7 text-richblack-50 text-[16px] select-none ">
+         {loading ? (
+            <div className=" w-[100vw] h-[90vh] flex flex-row mx-auto justify-center items-center">
+               <span className="loader"></span>
             </div>
+         ) : (
+            <div className=" w-[508px] flex flex-col py-2 px-9  ">
+               <h1 className=" text-3xl text-richblack-5 font-semibold mb-3 ">
+                  Verify Email
+               </h1>
+               <p className=" text-[17px] text-richblack-200 mb-3">
+                  A verification code has been sent to you. Enter the code below
+               </p>
 
-            <button type="submit" onClick={submitHandler}>
-              <CTAButton active={true} >Verify Email</CTAButton>
-            </button>
-          </form>
+               <form
+                  onSubmit={submitHandler}
+                  className="flex flex-col items-center gap-2"
+               >
+                  <div className="">
+                     <OtpInput
+                        value={otp}
+                        onChange={setOtp}
+                        numInputs={6}
+                        inputType="text"
+                        renderInput={(props) => (
+                           <input
+                              {...props}
+                              className=" w-[40px] bg-richblack-800 "
+                           />
+                        )}
+                        renderSeparator={<span style={{ width: "8px" }}></span>}
+                        inputStyle={{
+                           border: "1px solid",
+                           borderColor: "GrayText",
+                           borderRadius: "3px",
+                           width: "45px",
+                           height: "45px",
+                           fontSize: "20px",
+                           color: "#fff",
+                           textAlign: "center",
+                           fontWeight: "400",
+                           caretColor: "blue",
+                        }}
+                     />
+                  </div>
 
-          <div>
-            <Link className="" to={"/login"}>
-              <span className="flex flex-row items-center gap-3">
-                <FaArrowLeftLong />
-                Back to login
-              </span>
-            </Link>
-
-            <button
-              onClick={() => {
-                dispatch(sendOtp(signupData.email, navigate));
-              }}
-            >
-              Resend it
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+                  <div className=" w-full">
+                     <button
+                        className=" w-full"
+                        type="submit"
+                        onClick={submitHandler}
+                     >
+                        <CTAButton active={true}>Verify Email</CTAButton>
+                     </button>
+                  </div>
+                  <div className="flex justify-between items-center w-full">
+                     <Link className="" to={"/login"}>
+                        <div className="flex flex-row gap-2 items-center ">
+                           <FaArrowLeftLong />
+                           Back
+                        </div>
+                     </Link>
+                     <div>
+                        <button
+                           onClick={() => {
+                              dispatch(sendOtp(signupData.email, navigate));
+                           }}
+                        >
+                           Resend it
+                        </button>
+                     </div>
+                  </div>
+               </form>
+            </div>
+         )}
+      </div>
+   );
 };
 
 export default VerifyEmail;
