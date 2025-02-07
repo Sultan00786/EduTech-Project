d3ec5144f4e9928af117db972ea1449fe2a0843d
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-02-07 14:46:36 +0530

    feat: catalog ui changes and free course part is still remaining

--- src/components/core/Catalog/CourseCard.jsx
+++ src/components/core/Catalog/CourseCard.jsx
@@ -4,45 +4,46 @@ import { Link } from "react-router-dom";
 import GetAvgRating from "../../../utils/avgRating";
 
 function CourseCard({ course, Height }) {
-  // console.log(course);
+   // console.log(course);
 
-  const [avgReviewCount, setAvgReviewCount] = useState(0);
+   const [avgReviewCount, setAvgReviewCount] = useState(0);
 
-  useEffect(() => {
-    const count = GetAvgRating(course.ratingAndReviews);
-    setAvgReviewCount(count);
-  }, [course]);
-  return (
-    <div>
-      <Link to={`/courses/${course._id}`}>
-        <div>
-          <div>
-            <img
-              src={course?.thumbnail}
-              alt={course?.courseName}
-              className={`${Height} object-cover rounded-xl`}
-            />
-          </div>
-          <div>
-            <p>{course?.courseName}</p>
-            <p>
-              {course?.instructor?.firstName} {course?.instructor?.lastName}
-            </p>
-            <div className="flex gap-2 ">
-              <span>{avgReviewCount || 0}</span>
-              <RatingStars Review_Count={avgReviewCount || 0} />
-              <span className=" text-richblack-300 text-sm">
-                {course?.ratingAndReviews?.length} Ratings
-              </span>
+   useEffect(() => {
+      const count = GetAvgRating(course.ratingAndReviews);
+      setAvgReviewCount(count);
+   }, [course]);
+   return (
+      <div className="">
+         <Link to={`/courses/${course._id}`}>
+            <div>
+               <div>
+                  <img
+                     src={course?.thumbnail}
+                     alt={course?.courseName}
+                     className={`${Height} object-cover rounded-xl aspect-video`}
+                  />
+               </div>
+               <div>
+                  <p>{course?.courseName}</p>
+                  <p>
+                     {course?.instructor?.firstName}{" "}
+                     {course?.instructor?.lastName}
+                  </p>
+                  <div className="flex gap-2 ">
+                     <span>{avgReviewCount || 0}</span>
+                     <RatingStars Review_Count={avgReviewCount || 0} />
+                     <span className=" text-richblack-300 text-sm">
+                        {course?.ratingAndReviews?.length} Ratings
+                     </span>
+                  </div>
+                  <p className=" font-bold text-richblack-5 text-xl">
+                     Rs. {course?.price}
+                  </p>
+               </div>
             </div>
-            <p className=" font-bold text-richblack-5 text-xl">
-              Rs. {course?.price}
-            </p>
-          </div>
-        </div>
-      </Link>
-    </div>
-  );
+         </Link>
+      </div>
+   );
 }
 
 export default CourseCard;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -12,113 +12,130 @@ import CourseSlider from "../components/core/Catalog/CourseSlider";
 const { CATEGORIES_API } = categories;
 
 const Catalog = () => {
-  const { catalogName } = useParams();
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const [categoryDetails, setCategoryDetails] = useState();
+   const { catalogName } = useParams();
+   const navigate = useNavigate();
+   const [loading, setLoading] = useState(false);
+   const [categoryDetails, setCategoryDetails] = useState();
 
-  useEffect(() => {
-    async function getCategoryId() {
-      setLoading(true);
-      const allCategorys = await apiConnector("GET", CATEGORIES_API, null);
+   useEffect(() => {
+      async function getCategoryId() {
+         setLoading(true);
+         const allCategorys = await apiConnector("GET", CATEGORIES_API, null);
 
-      if (!allCategorys) {
-        toast.error("Error in fetching all Categories");
-        setLoading(false);
-        return;
+         if (!allCategorys) {
+            toast.error("Error in fetching all Categories");
+            setLoading(false);
+            return;
+         }
+
+         const currentCategory = allCategorys?.data?.allCategorys.filter(
+            (category) =>
+               category.name.split(" ").join("-").toLowerCase() === catalogName
+         );
+
+         const response = await getCatalogPageData({
+            categoryId: currentCategory[0]._id,
+         });
+
+         console.log("Response >> ", response);
+
+         if (response.length !== 0) setCategoryDetails(response);
+         else {
+            navigate("/");
+            toast.error("No Course available !!");
+         }
+         // console.log("Details >> ", categoryDetails);
+         setLoading(false);
       }
+      getCategoryId();
+   }, [catalogName]);
 
-      const currentCategory = allCategorys?.data?.allCategorys.filter(
-        (category) =>
-          category.name.split(" ").join("-").toLowerCase() === catalogName
+   if (loading) {
+      return (
+         <div className=" h-screen flex items-center justify-center">
+            <div className=" loader"></div>
+         </div>
       );
+   }
 
-      const response = await getCatalogPageData({
-        categoryId: currentCategory[0]._id,
-      });
-
-      console.log("Response >> ", response);
-
-      if (response.length !== 0) setCategoryDetails(response);
-      else {
-        navigate("/");
-        toast.error("No Course available !!");
-      }
-      // console.log("Details >> ", categoryDetails);
-      setLoading(false);
-    }
-    getCategoryId();
-  }, [catalogName]);
-
-  if (loading) {
-    return (
-      <div className=" h-screen flex items-center justify-center">
-        <div className=" loader"></div>
-      </div>
-    );
-  }
-
-  return (
-    <div className=" text-white w-full mx-auto ">
-      <div className=" w-full py-14 bg-richblack-800">
-        <div className=" w-11/12 mx-auto">
-          <div className=" w-11/12 mx-auto flex flex-col gap-3">
-            <div>
-              <span className=" text-sm text-richblack-100 w-fit">
-                Home / Catalog /{" "}
-              </span>
-              <span className=" text-sm text-yellow-50">
-                {categoryDetails?.selectedCetogry?.name}
-              </span>
+   return (
+      <div className=" text-white w-full mx-auto ">
+         <div className=" w-full py-14 bg-richblack-800">
+            <div className=" w-11/12 mx-auto">
+               <div className=" w-11/12 mx-auto flex flex-col gap-3">
+                  <div>
+                     <span className=" text-sm text-richblack-100 w-fit">
+                        Home / Catalog /{" "}
+                     </span>
+                     <span className=" text-sm text-yellow-50">
+                        {categoryDetails?.selectedCetogry?.name}
+                     </span>
+                  </div>
+                  <p className=" text-richblack-25 font-bold text-2xl">
+                     {categoryDetails?.selectedCetogry?.name}
+                  </p>
+                  <p className=" text-richblack-200 text-sm">
+                     {categoryDetails?.selectedCetogry?.description}
+                  </p>
+               </div>
             </div>
-            <p className=" text-richblack-25 font-bold text-2xl">
-              {categoryDetails?.selectedCetogry?.name}
-            </p>
-            <p className=" text-richblack-200 text-sm">
-              {categoryDetails?.selectedCetogry?.description}
-            </p>
-          </div>
-        </div>
+         </div>
+
+         <div className=" w-11/12 mx-auto mb-16">
+            <div className=" w-11/12 flex flex-col gap-5 mx-auto mt-9">
+               <p className=" text-richblack-5 font-bold text-4xl">
+                  Free Courses
+               </p>
+               <div>
+                  <CourseSlider
+                     courses={categoryDetails?.differentCategory?.courses}
+                  />
+               </div>
+            </div>
+
+            <div className=" w-11/12 flex flex-col gap-5 mx-auto mt-16">
+               <p className=" text-richblack-5 font-bold text-4xl">
+                  Top Courses in {categoryDetails?.selectedCetogry?.name}
+               </p>
+
+               <div>
+                  <CourseSlider
+                     courses={categoryDetails?.selectedCetogry?.courses}
+                  />
+               </div>
+            </div>
+
+            <div className=" w-11/12 flex flex-col gap-5 mx-auto mt-16">
+               <p className=" text-richblack-5 font-bold text-4xl">
+                  Frequently Bought
+               </p>
+
+               <div>
+                  <CourseSlider courses={categoryDetails?.mostSellingCourses} />
+               </div>
+            </div>
+
+            {/* <div className=" w-11/12 flex flex-col gap-5 mx-auto mt-24 mb-16">
+               <p className=" text-richblack-5 font-bold text-4xl">
+                  Frequently Bought
+               </p>
+               <div className=" grid grid-cols-1 lg:grid-cols-2 gap-10 py-8">
+                  {categoryDetails?.mostSellingCourses
+                     .slice(0, 4)
+                     .map((course, i) => (
+                        <CourseCard
+                           course={course}
+                           key={i}
+                           Height={"h-[400px]"}
+                        />
+                     ))}
+               </div>
+            </div> */}
+         </div>
+
+         <Footer></Footer>
       </div>
-
-      <div className=" w-11/12 mx-auto">
-        <div className=" w-11/12 flex flex-col gap-5 mx-auto mt-9">
-          <p className=" text-richblack-5 font-bold text-4xl">
-            Course to get you started
-          </p>
-          <div>
-            <CourseSlider courses={categoryDetails?.selectedCetogry?.courses} />
-          </div>
-        </div>
-
-        <div className=" w-11/12 flex flex-col gap-5 mx-auto mt-24">
-          <p className=" text-richblack-5 font-bold text-4xl">
-            Top Courses in {categoryDetails?.selectedCetogry?.name}
-          </p>
-          <div>
-            <CourseSlider
-              courses={categoryDetails?.differentCategory?.courses}
-            />
-          </div>
-        </div>
-
-        <div className=" w-11/12 flex flex-col gap-5 mx-auto mt-24 mb-16">
-          <p className=" text-richblack-5 font-bold text-4xl">
-            Frequently Bought
-          </p>
-          <div className=" grid grid-cols-1 lg:grid-cols-2 gap-10 py-8">
-            {categoryDetails?.mostSellingCourses
-              .slice(0, 4)
-              .map((course, i) => (
-                <CourseCard course={course} key={i} Height={"h-[400px]"} />
-              ))}
-          </div>
-        </div>
-      </div>
-
-      <Footer></Footer>
-    </div>
-  );
+   );
 };
 
 export default Catalog;
