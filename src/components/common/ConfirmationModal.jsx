b1d57379d6f109d3dfc5cf12e0e308cfad715b17
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-03-17 16:57:04 +0530

    feat: styling changes accourding to navbar responsiveness

--- src/components/common/ConfirmationModal.jsx
+++ src/components/common/ConfirmationModal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Iconbtn from "./Iconbtn";
 
-function ConfirmationModal({ modalData }) {
+function ConfirmationModal({ modalData, onClose = () => {} }) {
    return (
       <div className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm">
          <div className=" p-5 bg-richblack-900 flex flex-col gap-2 w-[300px] rounded-xl border-[1px] border-richblack-500 ">
@@ -11,7 +11,10 @@ function ConfirmationModal({ modalData }) {
             <p className=" text-richblack-200 text-sm">{modalData.text2}</p>
             <div className="flex items-center gap-2 mt-2">
                <Iconbtn
-                  onClick={modalData?.btn1Handler}
+                  onClick={() => {
+                     modalData?.btn1Handler();
+                     onClose();
+                  }}
                   text={modalData?.btn1Text}
                />
                <button
--- src/components/core/Auth/ProfileDropDown.jsx
+++ src/components/core/Auth/ProfileDropDown.jsx
@@ -8,7 +8,7 @@ import { IoIosArrowDown } from "react-icons/io";
 import { VscDashboard, VscSignOut } from "react-icons/vsc";
 import { ACCOUNT_TYPE } from "../../../utils/constants";
 
-const ProfileDropDown = () => {
+const ProfileDropDown = ({ onClose = () => {} }) => {
    const { user } = useSelector((state) => state.profile);
    const dispatch = useDispatch();
    const navigate = useNavigate();
@@ -16,33 +16,35 @@ const ProfileDropDown = () => {
    const [confirmationModal, setConfirmationModal] = useState(null);
 
    return (
-      <div onClick={() => setOpen(!open)} className="relative cursor-pointer">
-         <div className="flex items-center gap-x-1">
+      <div className="relative w-fit">
+         <div
+            onClick={() => setOpen(!open)}
+            className=" w-fit flex items-center gap-x-1 cursor-pointer"
+         >
             <img
                src={user?.image}
                alt={`profile-${user?.firstName}`}
-               className="w-[30px] aspect-square rounded-full object-cover"
+               className="w-[32px] md:w-[30px] aspect-square rounded-full object-cover border-[2px] p-[1px] border-richblack-700"
             />
             <IoIosArrowDown
-               className={`text-lg text-richblack-100 group-hover:text-white group-hover:rotate-180 transition-all duration-200 ${
-                  open ? "rotate-180" : ""
+               className={`text-lg text-richblack-100 group-hover:text-white group-hover:rotate-180 transition-all duration-300 ${
+                  open ? "-rotate-180" : ""
                } transition-all duration-200`}
             />
          </div>
 
          {open && (
             <div
-               className="absolute -left-[30%] top-[120%] z-[100] divide-y-[1px] divide-richblack-700
+               className="absolute left-6 md:-left-[30%] md:top-[120%] z-[100] divide-y-[1px] divide-richblack-700
                 overflow-hidden rounded-md border-[2px] border-richblack-700 bg-richblack-800
                 transition-all duration-200"
             >
-               {/* <div
-                  className="absolute right-[10%] -top-2 h-6 w-6 rotate-45 rounded bg-richblack-800
-            translate-y-[20%] border-l-[1px] border-t-[1px] border-richblack-700"
-               ></div> */}
-
                {user?.accountType === ACCOUNT_TYPE.STUDENT && (
-                  <Link to="/dashboard/enrolled-courses" className="block">
+                  <Link
+                     onClick={onClose}
+                     to="/dashboard/enrolled-courses"
+                     className="block"
+                  >
                      <div
                         className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100
                 hover:bg-richblack-700 hover:text-white transition-all duration-200"
@@ -88,7 +90,10 @@ const ProfileDropDown = () => {
 
          {/* Confirmation Modal */}
          {confirmationModal && (
-            <ConfirmationModal modalData={confirmationModal} />
+            <ConfirmationModal
+               onClose={onClose}
+               modalData={confirmationModal}
+            />
          )}
       </div>
    );
